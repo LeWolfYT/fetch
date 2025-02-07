d318f7f4ba4a5760a208fd03958d9b11704b70c6
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2025-02-07 13:09:48 -0500

    Update fetchnws.js

--- fetchnws.js
+++ fetchnws.js
@@ -4,9 +4,9 @@ function fetchnw(icao) {
   fetch("https://aviationweather.gov/api/data/metar?ids" + icao + "&format=json&taf=true")
   .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data);
       resp = data;
-    })
+    });
   return resp
 }
 var $module = {
